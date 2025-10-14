c050ecb3f31b6c2fe835d1ca2044c04bdfba771d
Samuel Bodenhemier <swb79@nau.edu>
2025-10-14 15:24:27 -0700

    Demo tailwind usage in svelte

---
+++ frontend/style/tailwind.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-screen border-b-1 shadow border-gray-400 pt-4 text-center">
+    <div class="text-4xl font-bold">Header</div>
+    <div class="flex justify-center gap-4 max-[400px]:gap-2 py-2">
+        <div class="navlink">NavLink1</div>
+        <div class="navlink">NavLink2</div>
+        <div class="navlink">NavLink3</div>
+        <div class="navlink">NavLink4</div>
+    </div>
+</div>
+<main>
+    <div class="max-w-6xl flex flex-col gap-4">
+        <h1 class="text-center text-2xl font-bold p-4">Welcome to the page</h1>
+        <div class="segment">
+            <h2 class="text-xl font-bold text-center">This is some content</h2>
+            <p class="p-4 text-red-800 text-sm">Here you can find some content for the page</p>
+            <div><img alt="example-img" class="pt-10 size-30 border-1 text-center" /></div>
+        </div>
+        <div class="segment">
+            <h2 class="text-xl font-bold text-center">This is some content</h2>
+            <p class="p-4 text-green-700">Here you can find some content for the page</p>
+            <div class="relative float-end"><img alt="example-img" class="pt-10 h-30 w-50 border-1 text-center" /></div>
+        </div>
+        <div class="segment">
+            <h2 class="text-xl font-bold text-center">This is some content</h2>
+            <p class="p-4 text-blue-600 text-lg">Here you can find some content for the page</p>
+            <div class="flex justify-center"><img alt="example-img" class="pt-10 w-30 h-50 border-1 text-center" /></div>
+        </div>
+    </div>
+</main>
